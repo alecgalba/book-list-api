e992fff897d8e444e7dfd989a544cfaab9436a4d
alecgalba <alecgalba@gmail.com>
2018-02-15 17:56:03 -0500

    added pages to serializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,3 +1,3 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :description, :amazon_id, :rating, :finished_on, :keywords
+  attributes :id, :title, :author, :description, :amazon_id, :rating, :finished_on, :keywords, :pages
 end
