2dc834dbe9cf58e73b51eb3a6685f9e4c80bca15
alecgalba <alecgalba@gmail.com>
2018-02-15 13:37:22 -0500

    migrated genre model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215181029) do
+ActiveRecord::Schema.define(version: 20180215183604) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20180215181029) do
     t.integer "rating"
     t.date "finished_on"
     t.text "keywords"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
